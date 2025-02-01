fff79aa235a681e775a3ae2224f2e231fa8db145
tsawyer <yungf4st@gmail.com>
2025-01-31 23:20:22 -0800

    added a PLAYER_SPEED constant and update the update method to move the player object forward and back.

--- player.py
+++ player.py
@@ -24,7 +24,15 @@ class Player(CircleShape):
             self.rotate(dt, -1)
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rotate(dt, 1)
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.move(dt)
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.move(dt)
 
     def rotate(self, dt, direction):
         self.rotation += PLAYER_TURN_SPEED * dt * direction
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
     
